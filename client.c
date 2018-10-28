72e110057d374f6cb0d0fcc6baf735c1679f914e
ProgVitor <vhferprog@gmail.com>
2018-10-28 18:22:58 +0000

    Cliente atualizado

--- client.c
+++ client.c
@@ -15,7 +15,8 @@
 int main(int argc, char *argv[])
 {
     char *var_nome=NULL;
-    int i;
+    int i,c;
+    c=0;
     server server;
 
     if(argc==1){
@@ -23,22 +24,31 @@ int main(int argc, char *argv[])
         exit(-1);
     }
 
-    while((i = getopt(argc, argv, "u:")) != -1){
+    busca_ambiente(&server);
+
+    while((i = getopt(argc, argv, "u:p:n:")) != -1){
         switch(i){
             case 'u':
                 var_nome = optarg;
-                busca_ambiente(&server);
                 if(verifica_user(server.MEDIT_FICHEIRO,var_nome,argv[0])==1){
-                    documento(var_nome, &server);
+                    c=1;                    
                 }
                 else{
                     fprintf(stderr,"%s: Esse utilizador nao existe\n",argv[0]);
                     exit(-1);
-                }
+                }   
                 break;
-            case '?':
+            case 'n':
+                server.MEDIT_NAME_PIPE_PRINCI=optarg;
+                break;
+            case 'p':
+                server.MEDIT_NUM_PIPES= atoi(optarg);
+                break;
+            default:
 				fprintf(stderr, "Opcao invalida!\n");
         }
-    } 
+    }    
+    if(c)
+        documento(var_nome, &server);
     exit(0);
 }
